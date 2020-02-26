d5a765ef946731f6098949f34f5dd6f2124e97b8
houjinkai <648073731@qq.com>
2020-02-26 21:52:41 +0800

    python 3 2020年2月26日21:52:19

--- baidusearch.py
+++ baidusearch.py
@@ -2,17 +2,29 @@
 from selenium import  webdriver
 import  time
 import csv
+import pandas as pd
+# import file
 import sys
 # reload(sys)
 # sys.setdefaultencoding("utf-8")
-#my_file='C:\\selenium\\data\\search.csv'
-# values=csv.reader(file(my_file,'rb'))
-values=csv.reader(open('C:\\selenium\\data\\search.csv','rb'))
-for search in values:
-    print (search[0])
-    driver = webdriver.Chrome()
-    driver.get("https://www.baidu.com")
-    driver.find_element_by_id("kw").send_keys(search[0])
-    driver.find_element_by_id("su").click()
-    time.sleep(3)
-    driver.quit()
+# my_file='C:\\selenium\\data\\search.csv'
+# values=csv.reader('my_file','rb')
+# values=csv.reader(open('C:\\selenium\\data\\search.csv','rb'))
+# print(values)
+# with open('C:\\selenium\\data\\search.csv','r') as csvfile:
+#     reader = csv.reader(csvfile)
+#     column1 = [row[2] for row in reader]
+#     print(column1)
+#     # d = pd.read_csv('C:\\selenium\\data\\search.csv', usecols=[])
+#     # print(d)
+column1=['hou','ggh']
+for search in column1:
+    print (search)
+    # driver = webdriver.Chrome()
+    # driver.get("https://www.baidu.com")
+    # driver.find_element_by_id("kw").send_keys(search)
+    # driver.find_element_by_id("su").click()
+    # time.sleep(3)
+    # driver.quit()
+a=(1,2,3,4)
+print(a)
---
+++ test/list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+"""
+@author: houjinkai
+@contact: xx@xx.com
+@software: PyCharm
+@file: list.py
+@time: 2020/2/18 21:35
+"""
+class test:
+    def prt(self):
+        print(self)
+        print(self.__class__)
+
+t=test()
+t.prt()
