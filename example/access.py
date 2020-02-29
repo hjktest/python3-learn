e462313294f7f8bbfcb5aca004fc9f195830e3fa
houjinkai <648073731@qq.com>
2020-02-29 15:27:19 +0800

    python 3 2020-2-29 15:27:08

--- example/access.py
+++ example/access.py
@@ -12,21 +12,22 @@ class Student(object):
     def __init__(self,name,score):
         self.name=name
         self.score=score
+# if.self.name update:self__name,self.__score但是已经无法从外部访问实例变量.__name和实例变量.__score了：
     def get_name(self):
-        return self._name
+        return self.name
     def get_score(self):
-        return self._score
+        return self.score
 
     def set_score(self,score):
         if 0<=score<=100:
-            self._score=score
+            self.__score=score
         else:
             raise ValueError('bad score')
 
     def get_grade(self):
-        if self._score>=90:
+        if self.__score>=90:
             return 'A'
-        elif self._score>=60:
+        elif self.__score>=60:
             return 'B'
         else:
             return 'C'
@@ -34,4 +35,4 @@ bart=Student('Bart simpson',59)
 print('bart.get_name()=',bart.get_name())
 bart.set_score(60)
 print('bart.get_score()=',bart.get_score())
-print('Do Not user bart.Student_name:',bart._Student_name)
+print('Do Not user bart.Student_name:',bart.__Student_name)
