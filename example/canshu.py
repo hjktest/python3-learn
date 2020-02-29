457b3b17063188c9e5f178d295902a160faba446
houjinkai <648073731@qq.com>
2020-02-29 11:04:19 +0800

    python 3 2020年2月26日21:52:19

---
+++ example/canshu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@author: houjinkai
+@contact: xx@xx.com
+@software: PyCharm
+@file: canshu.py
+@time: 2020/2/29 10:08
+"""
+def fl(a,b,c=0,*args,**kw):
+    print("a=",a,'b=',b,"c=",c,"args=","kw=",kw)
