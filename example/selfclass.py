b8856b78024877bc6884cd9acce0a7d518ca546e
houjinkai <648073731@qq.com>
2020-03-03 17:55:56 +0800

    python 3 2020年3月3日17:34:24

--- example/selfclass.py
+++ example/selfclass.py
@@ -39,3 +39,27 @@ class Fib1(object):
         return a
 f=Fib1()
 print(f[5])
+# 对于Fib却报错。原因是__getitem__()传入的参数可能是一个int，也可能是一个切片对象slice，所以要做判断：
+class Fib2(object):
+    def __getitem__(self, n):
+        if isinstance(n,int):#n是索引
+            a,b=1,1
+            for x in range(n):
+                a,b=b,a+b
+            return a
+        if isinstance(n,slice):
+                start=n.start
+                stop=n.stop
+                if start is None:
+                    start=0
+                a,b=1,1
+                L=[]
+                for x in range(stop):
+                    if x>=start:
+                        L.append(a)
+                    a,b=b,a+b
+                return L
+f1=Fib2()
+print(f1[0:5])
+print(f1[:10])
+print(f1[:10:2])
