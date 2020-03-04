c58406661b9e8cd025d31c4a93614f627b81fb21
houjinkai <648073731@qq.com>
2020-03-04 09:10:52 +0800

    python 3 2020年3月3日17:34:24

---
+++ example/mydict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@author: houjinkai
+@contact: xx@xx.com
+@software: PyCharm
+@file: mydict.py
+@time: 2020/3/3 20:52
+"""
+class Dict(dict):
+    def __int__(self,**kw):
+        super.__init__(**kw)
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise  AttributeError("DIc object has no attribute '%s'" % key)
+    def __setattr__(self, key, value):
+        self[key]=value
--- example/unittest.py
+++ example/unittest.py
@@ -8,14 +8,14 @@
 @file: unittest.py
 @time: 2020/3/3 20:31
 """
-class Dict(dict):
-    def __int__(self,**kw):
-        super.__init__(**kw)
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise  AttributeError("DIc object has no attribute '%s'" % key)
-    def __setattr__(self, key, value):
-        self[key]=value
+import unittest
+from example import mydict
+from mydict import Dict
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d=Dict(a=1,b='test')
+        self.assertEqual(d.a,1)
+        self.assertEqual(d.b,'test')
+        self.assertTrue(isinstance(d, dict))
+
 
