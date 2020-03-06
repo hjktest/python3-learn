9e2dac7cb453df57cdb8ca8a2acee402b64ff318
houjinkai <648073731@qq.com>
2020-03-06 18:30:45 +0800

    python 2020年3月6日18:26:19

--- example/lockthread.py
+++ example/lockthread.py
@@ -29,10 +29,6 @@ def change_it(n):
      balance=balance+n
      balance=balance-n
 
-def run_thread(n):
-    for i in range(100000):
-        change_it(n)
-
 t1=threading.Thread(target=run_thread,args=(5,))
 t2=threading.Thread(target=run_thread,args=(8,))
 t1.start()
